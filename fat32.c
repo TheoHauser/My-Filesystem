5d2007bae93e01ec728370ec92c6d5828825d754
TheoHauser <theo.hauser@temple.edu>
2015-12-03 19:56:37 -0500

    got somewhere today but having problems with createDirectory

--- fat32.c
+++ fat32.c
@@ -15,18 +15,6 @@
 #define ROOT 78
 #define FAT 2
 
-//function declarations
-void clearDrive();
-int firstByte(int cluster);
-short *getTimeDate();
-int firstAvailable();
-void formatDrive();
-void createDirTable();
-void createFATentry(int cluster, short next);
-void createDirEntry(char name[11], char attributes, short time, short date, short stCluster, long fileSize);
-FILE *createDirectory(char *path);
-
-
 //each FAT entry is 2 bytes, holds next cluster in file or special character 
 typedef struct FATentry{
 	short next;
@@ -34,7 +22,7 @@ typedef struct FATentry{
 
 //Each Directory table entry is 32 bytes
 typedef struct dirEntry{
-	char name[11];
+	char *name;
 	int rdonly : 1;
 	int hidden : 1;
 	int sysfil : 1;
@@ -48,14 +36,28 @@ typedef struct dirEntry{
 	long fileSize;
 }dirEntry;  
 
+//function declarations
+void clearDrive();
+int firstByte(int cluster);
+short *getTimeDate();
+int firstAvailable();
+void formatDrive();
+void createDirTable();
+void createFATentry(int cluster, short next);
+dirEntry *createDirEntry(char *namep, char attributes, short time, short date, short stCluster, long fileSize);
+dirEntry *createDirectory(char *path);
+
+//initialize file pointer
 FILE *drive;
 
+//initialize cluster "pointers"
 int currentDir;
 int parentDir;
 int currentCluster;
 int currentSpace;
 int currentOffset;
 
+//initialize time variables
 time_t rawtime;
 struct tm *timeinfo;
 
@@ -111,11 +113,12 @@ void clearDrive(){
 	fseek(drive, 0, SEEK_SET);
 	for(i=0; i<SECTORS; i++)
 		fwrite(nul, 512,1,drive);
+	fseek(drive,0, SEEK_SET);
 }
 
 int firstByte(int cluster){
-	cluster = cluster*512;
-	return (cluster-511);
+	cluster = (cluster-1)*512;
+	return (cluster);
 }
 
 short *getTimeDate(){
@@ -130,19 +133,18 @@ short *getTimeDate(){
         time = time+(timeinfo->tm_min<<5);
         time = time +(timeinfo->tm_sec);
 	*p = time; p++;
-        printf("%x\n", time);
 
 	//format date
 	short date = (short)((timeinfo->tm_year-80)<<9);
 	date = date + (timeinfo->tm_mon<<5);
 	date = date + (timeinfo->tm_mday);
 	*p = date; p++;
-	printf("%x\n", date);
 	return timedate;
 
 }
 
 void formatDrive(){
+	fseek(drive, 0, SEEK_SET);
 	currentDir = ROOT-RESERVED;
         currentCluster = currentDir;
         currentSpace = DATASIZE;
@@ -180,6 +182,7 @@ void formatDrive(){
         boot+=480; *boot = 0x55; boot++; *boot = 0xaa;
 
         //write bootblock to drive
+	fseek(drive, 0, SEEK_SET);
         fwrite(bootblock, 1, 512,drive);
 	free(bootblock);
 
@@ -217,27 +220,33 @@ void createFATentry(int cluster, short next){
 	fwrite(new, 2, 1, drive);
 	//seek back to previous location
 	fseek(drive, firstByte(cluster)+currentOffset, SEEK_SET);
+	free(new);
 }
 
-FILE *createDirectory(char *path){
+dirEntry *createDirectory(char *path){
 	int i, j;
 	dirEntry *entry = malloc(16*sizeof(dirEntry));
-	FILE *dir;
-	char *names[16];
+	dirEntry *h = entry;
+	dirEntry *dir = malloc(sizeof(dirEntry));
+	char *names[17];
 	names[0] = strtok(path, "/");
 	for(i = 1; names[i]!= NULL && i < 16; i++){
 		names[i] = strtok(NULL, "/");
+		if(names[i]==NULL)
+			i--;
 	}
+	printf("i = %d", i);
 	fseek(drive, firstByte(ROOT), SEEK_SET);
-	for(j = 0 ; j < i ; j++, entry++){
+	for(j = 0 ; j < i-1 ; entry++){
 		fread(entry, 32, 1, drive);
 		currentOffset+=32;
-		if(entry->name==names[j] && j!=i){
+		if(entry->name==names[j]){
 			fseek(drive,firstByte(entry->stCluster-currentCluster),SEEK_CUR);
 			currentCluster = entry->stCluster; currentOffset = 0;
 			parentDir = currentDir; currentDir = currentCluster; 
+			j++;
 		}
-		else if(entry->stCluster == 0 && j!=i){
+		else if(entry->stCluster == 0 && j!=i-1){
 			printf("path not found\n");
 			return NULL;
 		}
@@ -245,25 +254,76 @@ FILE *createDirectory(char *path){
 			printf("path is a file and not a subdirectory\n");
 			return NULL;
 		}
-		else if(currentOffset = 512){
-			createFATentry(currentDir, firstAvailable());	
+		else if(currentOffset == 512){
+			printf("No space in current directory table");
+			return NULL;
 		}
 	}
-	//put in hidden files . and ..
+	//Do I need to create . and ..?
 	
+	//get time
 	short *timeDate = getTimeDate();
 	short date = *(timeDate+1);
 	char attributes = 0x08;
 
-	createDirEntry(names[j-1], attributes, *timeDate, date, currentCluster, BLOCKSIZE*10);
+	dir = createDirEntry(names[j], attributes, *timeDate, date, currentCluster, BLOCKSIZE*10);
 	createDirTable();
 
-	free(entry);
+	free(h);
+
 	return dir;
 }
 
-void createDirEntry(char *name, short attributes, short time, short date, int stCluster, long filesize){
-	dirEntry entry = malloc(sizeof(dirEntry));
+dirEntry *createDirEntry(char *namep, char attributes, short time, short date, short stCluster, long filesize){
+	dirEntry *entry = malloc(sizeof(dirEntry));
+	dirEntry *check = malloc(sizeof(dirEntry));
+	int numEntries;
+	//get name and update entry
+	char *name = namep;	
+	entry->name = name;
 
-	return entry;	
+	//set attributes
+	if(attributes>=128){
+		attributes -=128;
+		entry->rdonly = 1;
+	}
+	if(attributes>=64){
+		attributes -= 64;
+		entry->hidden = 1;
+	}
+	if(attributes>=32){
+		attributes -= 32;
+		entry->sysfil = 1;
+	}
+	if(attributes>=16){
+		attributes -= 16;
+		entry->volLabel = 1;
+	}
+	if(attributes>=8){
+		attributes -= 8;
+		entry->subdir = 1;
+	}
+	if(attributes>=4){
+		attributes -= 4;
+		entry->archive = 1;
+	}
+	
+	//set time&date
+	entry->time = time;
+	entry->date = date;
+
+	//set starting cluster and filesize
+	entry->stCluster = stCluster;
+	entry->fileSize = filesize;
+
+	//seek to currentdir to wirte entry
+	fseek(drive, firstByte(currentDir+RESERVED), SEEK_SET);
+
+	//find open entry
+	fread(check, 32, 1, drive);
+	while(check->stCluster!=0)
+		fread(check, 32, 1, drive);
+	fwrite(entry, 32, 1, drive);
+	fseek(drive, firstByte((entry->stCluster+RESERVED)), SEEK_SET);
+	return entry;
 }
